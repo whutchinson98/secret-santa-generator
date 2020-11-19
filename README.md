fee5c3ff185c5f11ad1057b92057dd4b5a1db628
Will Hutchinson <willhutchinson2.0@gmail.com>
2020-11-19 09:38:49 -0500

    Secret Santa Generator

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
+
 # secret-santa-generator
+
 Secret Santa Generator that emails each participant who they are Santa for.
+
+  
+  
+
+## To Run
+
+1. npm install
+
+2. create `.env` file in main directory and place environment variables `EMAIL` and `EMAIL_PASSWORD` for the email and password you will use to send the secret santa emails
+
+3. create `participants.json` file in main directory. The structure of the file is shown below
+
+		[
+			{
+				"name" : "persons name"
+				"email" : "persons email"
+			},
+			...
+		]
+
+4. To run use command `npm run start`
---
+++ index.js
@@ -0,0 +1,69 @@
+require('dotenv').config()
+const nodemailer = require('node-mailer')
+const participants = require('./participants.json')
+const EMAIL = process.env.EMAIL
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD
+
+
+const emailHelper = async (participantIndex, santaFor) => {
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: '465',
+        secure: true,
+        protocol: 'ssl',
+        auth: {
+          user: EMAIL,
+          pass: EMAIL_PASSWORD
+        }
+      })
+    
+      const message = `You are the secret santa for ${santaFor.name}`
+      const mailOptions = {
+        from: EMAIL,
+        to: participants[participantIndex].email,
+        subject: 'Secret Santa',
+        text: message
+      }
+    
+      try {
+        await transporter.sendMail(mailOptions)
+        return true
+      } catch (error) {
+        console.log(error)
+        return false
+      }
+}
+
+const generateSecretSanta = () => {
+    const participantsMap = {}
+    for(let i = 0; i < participants.length; i++){
+        const participant = participants[i]
+        let random = Math.floor(Math.random() * participants.length)
+        let reset = false
+
+        while(random in participantsMap || i === random){
+            if(participantsMap.length === participant.length - 1){
+                i = 0
+                participantsMap = {}
+                reset = true
+                break
+            }
+            random = Math.floor(Math.random() * participants.length)
+        }
+        if(!reset){
+            participantsMap[random] = participant
+        }
+    }
+    
+    for(let i = 0; i < participantsMap.length; i++){
+        emailSent = await emailHelper(i,participantsMap[i])
+
+        if(!emailSent){
+            console.log("Error occured please see console output for more details.")
+            return
+        }
+    }
+}
+
+
+generateSecretSanta()
